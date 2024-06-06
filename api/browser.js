fd32372ffd4e4ccc94882829cebeb83fec047a79
Bloxxy213 <jijaprogamer@gmail.com>
2024-06-06 17:04:16 +0300

    studio bug fixes

--- api/browser.js
+++ api/browser.js
@@ -1,226 +1,276 @@
+let sleep = (ms) => new Promise(r => setTimeout(r, ms))
 
-import * as fs from "fs"
-import * as path from "path"
-import got from "got"
-
-import { calculateRequestSize, calculateResponseSize } from 'puppeteer-bandwidth-calculator';
-
-import { GenerateFingerprint, LaunchBrowser } from "playwright-anti-fingerprinter";
-
-import pageClass from "./page.js"
-
-import { dirname } from 'path';
-import { createRequire } from 'module';
-
-import { fileURLToPath } from 'url';
-
-let __dirname = dirname(fileURLToPath(import.meta.url));
-let require = createRequire(import.meta.url);
-
-let extensions = fs.readdirSync(path.join(__dirname, "/defaultExtensions"))
-    .map((v) => v = path.join(__dirname, "/defaultExtensions/", v))
-    .map((v) => v = require(v))
-
-function fingerprintGenerator() {
-    return GenerateFingerprint("firefox", {
-        webgl_vendor: (e) => e.includes("Google Inc."),
-        webgl_renderer: (e) => true,
-        language: (e) => e.includes("en"),
-        userAgent: (e) => e.includes("Windows"),
-        viewport: (e) => e.width > 1000 && e.height > 800 && e.width < 2000 && e.height < 2000,
-        cpu: (e) => e <= 24 && e >= 4,
-        memory: (e) => true,
-        compatibleMediaMime: (e) => e.audio.includes("aac") && e.video["mp4"] && e.video.mp4.length > 0,
-        canvas: (e) => true,
-    })
+async function text(el) {
+    return await el.evaluate(e => e.innerText.trim())
 }
 
-async function shouldProxyRequest(page, request) {
-    return 2;
-    try {
-        let acceptedCookies = ["DEVICE_INFO", "VISITOR_INFO1_LIVE", "GPS"]
+class googleContext {
+    #page = {}
+    #parent = {}
+    #extra = {}
+    currentAccount = {}
+    #browser = {}
 
-        let page_url = page.url()
-        let url = request.url()
-        let currentCookies = await page.context().cookies()
-        let type = request.resourceType()
-
-        if (url.startsWith("data:image")) return 1
-        if (url.includes("gstatic")) return 1
-
-        let isLoggedIn = false
-
-        for (let cookie of currentCookies) {
-            if (acceptedCookies.includes(cookie.name)) {
-                isLoggedIn = true
-                break
-            }
-        }
-
-        if (!isLoggedIn && url.includes("googlevideo.com") && !page_url.includes("/shorts/")) return 3
-
-        if (request.method() == "GET") {
-            let isDocument = type == "document" || type == "script" || type == "manifest" || type == "stylesheet"
-
-            //if (bannedResourceTypes.includes(type)) return 3
-            //if (url.includes("fonts.")) return 3
-
-            if (isDocument && type == "document") return 2
-            if (isDocument) return 1
-        }
-
-        return 2
-    } catch (err) {
-        console.error(err)
-        return 3
-    }
-}
-
-class YoutubeSelfbotBrowser {
-    opts = {}
-    eventStore = {}
-    context = {}
-    extra = {}
-    ipInfo = {}
-    #firstPageCreated = false
-
-    constructor(opts, extra) {
-        this.opts = opts
-        this.extra = extra
+    constructor(page, parent, extra, browser) {
+        this.#page = page
+        this.#parent = parent
+        this.#extra = extra
+        this.#browser = browser
     }
 
-    isConnected() { return this.context.isConnected(...arguments) }
-    pages() { return this.context.browserContexts(...arguments) }
-    process() { return this.context.process(...arguments) }
-    target() { return this.context.target(...arguments) }
-    targets() { return this.context.targets(...arguments) }
-    userAgent() { return this.context.userAgent(...arguments) }
-    version() { return this.context.version(...arguments) }
-    waitForTarget() { return this.context.waitForTarget(...arguments) }
-    wsEndpoint() { return this.context.wsEndpoint(...arguments) }
-    close() { return this.context.close(...arguments) }
-    addCookies() { return this.context.addCookies(...arguments) }
+    setup() {
+        return new Promise(async (resolve) => {
+            await this.#page.goto(`https://myaccount.google.com/email`)
+            let el = await Promise.race([
+                this.#page.waitForSelector(`#identifierId`),
+                this.#page.waitForSelector(`.mMsbvc`),
+            ])
 
-    async setup() {
-        return new Promise(async (resolve, reject) => {
-            let fingerprint = this.extra.fingerprint || {
-                ...fingerprintGenerator(),
-                proxy: this.extra.proxy
+            let emailBox = await this.#page.$(`#identifierId`)
+            let currentEmail
+
+            if (!emailBox) {
+                currentEmail = await text(el)
             }
 
-            this.fingerprint = fingerprint;
+            if (emailBox) {
+                this.currentAccount = {
+                    email: "",
+                    password: "",
+                    cookies: [],
+                    formatted_cookies: "",
+                    loggedIn: false,
+                }
+            } else {
+                let currentCookies = await this.#parent.getCookies()
 
-            let opts = {
-                ...this.opts,
-                serviceWorkers: "block",
-                bypassCSP: true,
-            }
-
-
-            const { browser, ipInfo } = await LaunchBrowser("firefox", opts, fingerprint)
-
-            this.browser = browser
-            this.context = browser
-            this.ipInfo = ipInfo
-
-            this.context.setDefaultTimeout(this.extra.timeout)
-            this.context.setDefaultNavigationTimeout(this.extra.timeout)
-
-            //await (await this.context.newPage()).goto("about:blank") // making initial page
-
-            for (let extension of extensions) {
-                if (await extension.verify(this.extra)) {
-                    await this.context.addInitScript(extension.code).catch(reject)
+                this.currentAccount = {
+                    email: currentEmail,
+                    password: "",
+                    cookies: currentCookies,
+                    formatted_cookies: await this.#parent.getFormattedCookies(),
+                    loggedIn: true,
                 }
             }
 
-            await this.context.addInitScript(() => {
-                let DateNow = Date.now()
-                let DateYear = DateNow + 31536000000
-                localStorage.setItem('yt-player-quality', `{\"data\":\"{\\\"quality\\\":144,\\\"previousQuality\\\":240}\",\"expiration\":${DateYear},\"creation\":${DateNow}}`);
-                localStorage.setItem('yt-player-volume', `{"data":"{\\"volume\\":0,\\"muted\\":false}","expiration":${DateYear},"creation":${DateNow}}`);
-            }).catch(reject)
-
-            resolve()
+            resolve(this.currentAccount)
         })
     }
 
-    clearStorage() {
+    async login(accountInfo = {}, cookies) {
         return new Promise(async (resolve, reject) => {
             try {
-                const [page] = await Promise.all([
-                    this.context.waitForEvent('page'),
-                    (await this.context.pages())[0].evaluate(() => window.open('about:blank'))
-                ]);
-                //const page = await this.context.newPage()
+                await this.#page.goto(`https://myaccount.google.com/email`)
+                let el = await Promise.race([
+                    this.#page.waitForSelector(`#identifierId`),
+                    this.#page.waitForSelector(`.mMsbvc`),
+                ])
 
-                await page.context().clearCookies();
+                if (typeof cookies == "string" || typeof cookies == "object") {
+                    await this.#parent.clearCookies()
+                    await this.#parent.setCookies(cookies)
+                    await this.#page.goto(`https://myaccount.google.com/email`)
 
-                await page.goto("https://www.youtube.com");
-                await page.evaluate(() => localStorage.clear());
-
-                await page.close();
-
-                resolve();
-            } catch (err) {
-                reject(err);
-            }
-        })
-    }
-
-    on(name, event) {
-        this.eventStore[name] = event
-    }
-
-    emit() {
-        let args = Object.values(arguments)
-
-        let name = args.shift()
-        let event = this.eventStore[name]
-
-        if (event) {
-            event(...args)
-        }
-    }
-
-    async newPage() {
-        const [page] = await Promise.all([
-            this.context.waitForEvent('page'),
-            (await this.context.pages())[0].evaluate(() => window.open('about:blank'))
-        ]);
-
-        //const page = await this.context.newPage()
-
-        if (!this.#firstPageCreated) {
-            this.#firstPageCreated = true;
-            (await this.context.pages())[0].close()
-        }
-
-        let pgClass = new pageClass(page, this.extra, this)
-        await pgClass.initPage()
-
-        page.on("response", async (res) => {
-            let req = res.request()
-
-            let isVideo = (await req.url()).includes("googlevideo.com")
-
-            if (isVideo) {
-                if (pgClass.__onContinue) {
-                    pgClass.__onContinue()
-                    pgClass.__onContinue = undefined
+                    el = await Promise.race([
+                        this.#page.waitForSelector(`#identifierId`),
+                        this.#page.waitForSelector(`.mMsbvc`),
+                    ])
                 }
-            }
 
-            let shouldCalculateRequestSize = await shouldProxyRequest(page, req) == 2
+                let emailBox = (await this.#page.$$(`#identifierId`))[0]
+                let currentEmail
 
-            if (shouldCalculateRequestSize) {
-                this.emit("bandwith", pgClass.id, "download", await calculateRequestSize(req))
-                this.emit("bandwith", pgClass.id, "upload", await calculateResponseSize(res))
+                if (!emailBox) {
+                    currentEmail = await text(el)
+                }
+
+                if (emailBox || currentEmail !== accountInfo.email) {
+                    if (!accountInfo.email || !accountInfo.password) {
+                        this.#browser.emit("loginFailed", this.#parent.id, {
+                            header: "No account information given",
+                            instructions: "Please provide both email and password for the account",
+                        })
+
+                        reject("No account information given")
+                    }
+
+                    if (currentEmail !== accountInfo.email && currentEmail && accountInfo.email && accountInfo.email.length > 1) {
+                        await this.#parent.clearCookies()
+                        await this.#page.goto(`https://myaccount.google.com/email`)
+                        await this.#page.waitForSelector(`#identifierId`)
+                    }
+
+                    // email
+
+                    await this.#page.click(`#identifierId`)
+                    await sleep(1000)
+                    await this.#page.type(`#identifierId`, accountInfo.email, { delay: 75 })
+
+                    // continue
+                    await this.#page.waitForSelector(`#identifierNext`, { visible: true })
+                    await this.#page.click(`#identifierNext`)
+
+                    await Promise.race([
+                        this.#page.waitForSelector(`xpath=//*[@id="yDmH0d"]/c-wiz/div[2]/div[2]/div/div[1]/div/form/span/section/div/div/div/div[2]`),
+                        this.#page.waitForSelector(`#password`),
+                    ])
+
+                    let pel = await this.#page.$(`xpath=//*[@id="yDmH0d"]/c-wiz/div[2]/div[2]/div/div[1]/div/form/span/section/div/div/div/div[2]`)
+                    if (pel) {
+                        await sleep(500)
+
+                        let hEl = await this.#page.$(`#headingText > span`)
+
+                        let header = await text(hEl)
+                        let instructions = await text(pel[0])
+
+                        this.#browser.emit("loginFailed", this.#parent.id, {
+                            header,
+                            instructions,
+                        })
+
+                        return reject(header)
+                    }
+
+                    await sleep(500)
+
+                    // password
+
+                    let selector = await this.#page.waitForSelector(`#password`, { visible: true })
+                    await selector.click()
+                    await sleep(1000)
+                    await this.#page.type(`#password`, accountInfo.password, { delay: 75 })
+
+                    // continue
+
+                    await this.#page.waitForSelector(`#passwordNext`, { visible: true })
+                    await this.#page.click(`#passwordNext`)
+
+                    await Promise.race([
+                        this.#page.waitForSelector(`div[jsname="bySMBb"]`),
+                        this.#page.waitForSelector(`div[jsname="B34EJ"]`),
+                        this.#page.waitForSelector(`[data-challengetype="12"]`),
+                        this.#page.waitForSelector(`#identifierId`),
+                        this.#page.waitForSelector(`.mMsbvc`),
+                        this.#page.waitForSelector(`xpath=//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/div[1]/div/form/span/div[1]/div[2]/div[2]/span`),
+                        this.#page.waitForSelector(`xpath=//*[@id="view_container"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/span/figure/samp`),
+                    ])
+
+                    if (await this.#page.$(`[data-challengetype="12"]`)) {
+                        await Promise.all([
+                            this.#page.waitForNavigation({ waitUntil: "networkidle2" }),
+                            this.#page.click(`[data-challengetype="12"]`)
+                        ]);
+
+                        await sleep(1500);
+                        await this.#page.type(`#knowledge-preregistered-email-response`, accountInfo.recovery, { delay: 75 })
+                        await this.#page.click(`button`)
+
+                        await sleep(1000);
+                    }
+
+                    let pInc = await this.#page.$$(`xpath=//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/div[1]/div/form/span/div[1]/div[2]/div[2]/span`)
+                    let pInc2 = await this.#page.$$(`div[jsname="B34EJ"]`)
+
+                    if (pInc[0] || pInc2[0]) {
+                        await sleep(500)
+                        let instructions = await text(pInc[0] || pInc2[0])
+
+                        this.#browser.emit("loginFailed", this.#parent.id, {
+                            header: "wrong password",
+                            instructions,
+                        })
+
+                        return reject("wrong password")
+                    }
+
+                    let code = await this.#page.$(`xpath=//*[@id="view_container"]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/span/figure/samp`)
+
+                    if (code) {
+                        await sleep(500)
+                        code = parseInt(await text(code[0]))
+                        let handling = false
+
+                        await Promise.all([
+                            new Promise(async (resolve, reject) => {
+                                this.#browser.on("handlingCode", (isHandling) => {
+                                    handling = true
+                                    if (isHandling) {
+                                        resolve()
+                                    } else {
+                                        reject()
+                                    }
+                                })
+
+                                setTimeout(() => {
+                                    if (!handling) {
+                                        reject(`login code not being handled`)
+                                    }
+                                }, 5000)
+                            }),
+
+                            this.#browser.emit("loginCode", this.#parent.id, code)
+                        ])
+
+                        await new Promise((resolve, reject) => {
+                            this.#browser.on("cancelCodeHandling", reject)
+                            this.#browser.on("codeHandled", resolve)
+                        })
+                    }
+
+                    if (await this.#page.$(`button[jsname="bySMBb"]`)) {
+                        await Promise.all([
+                            this.#page.waitForNavigation({ waitUntil: "networkidle2" }),
+                            this.#page.click(`button[jsname="bySMBb"]`)
+                        ])
+                    }
+
+                    if (await this.#page.$(`#yDmH0d > c-wiz > div > div > div > div.L5MEH.Bokche.ypEC4c > div.lq3Znf > div:nth-child(1) > button`)) {
+                        await Promise.all([
+                            this.#page.waitForNavigation({ waitUntil: "networkidle2" }),
+                            this.#page.click(`#yDmH0d > c-wiz > div > div > div > div.L5MEH.Bokche.ypEC4c > div.lq3Znf > div:nth-child(1) > button`)
+                        ])
+                    }
+
+                    await this.#page.waitForSelector(`.mMsbvc`)
+
+                    this.currentAccount = {
+                        ...accountInfo,
+                        cookies: await this.#parent.getCookies(),
+                        formatted_cookies: await this.#parent.getFormattedCookies(),
+                        loggedIn: true,
+                    }
+
+                    resolve(this.currentAccount)
+                } else {
+                    this.currentAccount = {
+                        email: currentEmail,
+                        password: "",
+                        cookies: await this.#parent.getCookies(),
+                        formatted_cookies: await this.#parent.getFormattedCookies(),
+                        loggedIn: true,
+                    }
+                }
+
+                resolve(this.currentAccount)
+            } catch (err) {
+                reject(err)
             }
         })
+    }
 
-        return pgClass
+    async logout() {
+        await this.#parent.clearCookies()
+        this.currentAccount = {
+            email: "",
+            password: "",
+            cookies: [],
+            formatted_cookies: "",
+            loggedIn: false,
+        }
+
+        return this.currentAccount
     }
 }
 
-export default YoutubeSelfbotBrowser;
+export default googleContext
